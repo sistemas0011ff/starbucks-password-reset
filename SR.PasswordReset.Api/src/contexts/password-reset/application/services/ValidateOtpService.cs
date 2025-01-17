ba8f3b2400cf883aa6abc64e88c8f925b1628623
sistemas0011ff <efajardogutierrez@hotmail.com>
2025-01-17 10:21:54 -0500

    test deploy

--- SR.PasswordReset.Api/src/contexts/password-reset/application/services/ValidateOtpService.cs
+++ SR.PasswordReset.Api/src/contexts/password-reset/application/services/ValidateOtpService.cs
@@ -79,7 +79,7 @@ namespace SR.PasswordReset.Api.src.contexts.password_reset.application.services
                 return new ServiceResponse(
                     isSuccess: false,
                     code: "INTERNAL_ERROR",
-                    userMessage: "Error interno del servidor",
+                    userMessage: "Error del servicio, comunicarse con el administrador del sistema",
                     detailMessage: ex.Message
                 );
             }
